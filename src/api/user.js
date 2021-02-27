c7b7e8954c73a8e5b662e10e2b387f5b49f0750d
1234 <1234@qq.com>
2021-02-27 19:26:40 +0800

    首页完成

---
+++ src/api/user.js
@@ -0,0 +1,35 @@
+/**
+ * 用户相关的请求模块
+ */
+import request from '@/utils/request'
+// 用户登入
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
+// 发送验证码
+export const getSmsCode = mobile => {
+  return request({
+    method: 'GET',
+    url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+/**
+ * 获取用户自己的信息
+ */
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
+  })
+}
+// 获取用户频道列表
+export const getUserChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/channels'
+  })
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,41 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
-const routes = []
+// 路由表
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    // name: 'layout', // 如果父路由有默认子路由,那它的name没有意义
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 默认子路由
+        name: 'home',
+        component: () => import('@/views/home/index.vue')
+      },
+      {
+        path: 'qa',
+        name: 'qa',
+        component: () => import('@/views/qa/index.vue')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/video/index.vue')
+      },
+      {
+        path: 'my',
+        name: 'my',
+        component: () => import('@/views/my/index.vue')
+      }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
